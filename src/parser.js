06a4a50502d8258bc1150b80cc2a4a9dcf019ffb
Tom Rogers <3911927+tomprogers@users.noreply.github.com>
2018-02-17 22:50:20 -0600

    first stage of better handling of quoted field names & colons

--- src/parser.js
+++ src/parser.js
@@ -6,22 +6,26 @@
 	}
  */
 
-const trace = false // enable very granular debugging
+const trace = true // enable very granular debugging
 
 const CHAR_QUOTE = '"'
 const CHAR_BACKSLASH = '\\'
 const CHAR_SPACE = ' '
 const CHAR_COLON = ':'
 
-const QUOTE              = 0x0001
-const QUOTED_QUOTE       = 0x0010
-const ESCAPED_QUOTE      = 0x0100
-const BACKSLASH          = 0x1000
-const QUOTED_BACKSLASH   = 0x0011
-const ESCAPED_BACKSLASH  = 0x0101
-const SPACE              = 0x1001
-const QUOTED_SPACE       = 0x0110
-const ESCAPED_SPACE      = 0x1010
+const QUOTE              = 'QUOTE' //0x0001
+const QUOTED_QUOTE       = 'QUOTED_QUOTE' //0x0010
+const ESCAPED_QUOTE      = 'ESCAPED_QUOTE' //0x0100
+const BACKSLASH          = 'BACKSLASH' //0x1000
+const QUOTED_BACKSLASH   = 'QUOTED_BACKSLASH' //0x0011
+const ESCAPED_BACKSLASH  = 'ESCAPED_BACKSLASH' //0x0101
+const SPACE              = 'SPACE' //0x1001
+const QUOTED_SPACE       = 'QUOTED_SPACE' //0x0110
+const ESCAPED_SPACE      = 'ESCAPED_SPACE' //0x1010
+const COLON              = 'COLON'
+const QUOTED_COLON       = 'QUOTED_COLON'
+const ESCAPED_COLON      = 'ESCAPED_COLON'
+
 
 /**
  * given a single string that (presumably), separate it into /(field:)?"?value"?/ pairs
@@ -39,21 +43,20 @@ export default (input) => {
 	buffer until and unless a "special character" is encountered
 	
 	special characters are:
-		space - search terms are separated by spaces (unless those spaces are escaped or occur in a quoted phrase
+		space - search terms are separated by spaces (unless those spaces are escaped or occur in a quoted phrase)
 		quote - begins or ends a quoted phrase; within a quoted phrase, spaces are not treated as delimiters
 		backslash - used to escape special characters so they are treated as literal values
 		colon - the first unescaped colon signals that the term is a "key:value" construction instead of merely "value" (this is done via regex once basic separation is complete)
 	 */
 	
-	let currentTermString = '' // we build this up a char at a time, until we detect a literal, unquoted term separator
+	let currentTermParts = [] // there will be 1 or 2 term parts; this is how we tell diff between field name and term value
+	let currentAtomString = '' // we build this up character by character until we find a character that ends an atom (a space, a colon, a quote)
 	let insideQuotes = false // will be true when we detect the beginning of a quoted phrase
 	let nextCharIsEscaped = false // will be true for the one character immediately after any '\'
-
+	
 	for(let i = 0, iMax = input.length; i < iMax; i++) {
 		let thisChar = input.charAt(i)
 		let TYPE = 'OTHER'
-		
-		trace && console.log(`  >${thisChar}<`)
 		
 		// detection of quotes
 		if(thisChar === CHAR_QUOTE && !insideQuotes && !nextCharIsEscaped) {
@@ -82,6 +85,17 @@ export default (input) => {
 			TYPE = ESCAPED_SPACE
 		}
 		
+		// detection of colons
+		else if(thisChar === CHAR_COLON && !insideQuotes && !nextCharIsEscaped) {
+			TYPE = COLON
+		} else if(thisChar === CHAR_COLON && insideQuotes && !nextCharIsEscaped) {
+			TYPE = QUOTED_COLON
+		} else if(thisChar === CHAR_COLON && nextCharIsEscaped) {
+			TYPE = ESCAPED_COLON
+		}
+		
+		trace && console.log(`  >${thisChar}< ${TYPE}`)
+		
 		// at this point it's safe to reset the escape-tracker, because we've just finished honoring it (if true)
 		nextCharIsEscaped = false
 		
@@ -92,15 +106,13 @@ export default (input) => {
 				insideQuotes = true
 				break
 			
-			case QUOTED_QUOTE: // end phrase & term
-				trace && console.log('  end phrase and term')
+			case QUOTED_QUOTE: // end phrase
+				trace && console.log('  end phrase')
 				insideQuotes = false
-				terms.push(currentTermString)
-				currentTermString = ''
 				break
 			
 			case ESCAPED_QUOTE: // copy char
-				currentTermString += thisChar
+				currentAtomString += thisChar
 				break
 			
 			case BACKSLASH: // begin escape
@@ -114,30 +126,54 @@ export default (input) => {
 				break
 			
 			case ESCAPED_BACKSLASH: // copy char
-				currentTermString += thisChar
+				currentAtomString += thisChar
 				break
 			
-			case SPACE: // end term
-				trace && console.log('  end term')
-				terms.push(currentTermString)
-				currentTermString = ''
+			case SPACE: // end atom & term
+				trace && console.log('  end atom & term')
+				currentTermParts.push(currentAtomString)
+				terms.push(currentTermParts)
+				currentTermParts = []
+				currentAtomString = ''
 				break
 			
 			case QUOTED_SPACE: // copy char
-				currentTermString += thisChar
+				currentAtomString += thisChar
 				break
 			
 			case ESCAPED_SPACE: // copy char
-				currentTermString += thisChar
+				currentAtomString += thisChar
 				break
 			
+			case COLON: // end first atom, or copy
+				trace && console.log('  end first atom or copy')
+				if(currentTermParts.length === 0) {
+					currentTermParts.push(currentAtomString)
+					currentAtomString = ''
+				} else {
+					currentAtomString += thisChar
+				}
+				break
+			
+			case QUOTED_COLON:
+				currentAtomString += thisChar
+				break
+			
+			case ESCAPED_COLON:
+				currentAtomString += thisChar
+				break
+			
+			
 			default: //copy char
-				currentTermString += thisChar
+				currentAtomString += thisChar
 				break
 		}
 	}
 	
-	terms.push(currentTermString)
+	currentTermParts.push(currentAtomString)
+	terms.push(currentTermParts)
+	
+	trace && console.log(`terms`, terms)
 	
 	// remove any empty terms from the list (this is common after quoted terms)
 	let realTerms = []
