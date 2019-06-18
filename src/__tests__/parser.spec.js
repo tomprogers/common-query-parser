b47ed04a2ac2b9cee2c935a4675a9724d510eff7
Tom Rogers <tomprogers@users.noreply.github.com>
2019-06-18 10:33:50 -0500

    added more unit tests based on legacy code

--- src/__tests__/parser.spec.js
+++ src/__tests__/parser.spec.js
@@ -1,25 +1,24 @@
 import parseQuery from '../parser.js'
 
 
-describe(`parser`, () => {
+describe(`parseQuery( input )`, () => {
 
 
 	it(`handles a single term`, () => {
 		expect(
-			parseQuery('fiend')
+			parseQuery('ulysses')
 		).toEqual(
-			[{ value: 'fiend' }]
+			[{ value: 'ulysses' }]
 		)
 	})
 
 
 	it(`handles multiple terms`, () => {
 		expect(
-			parseQuery(`one two three`)
+			parseQuery(`pax romana`)
 		).toEqual([
-			{ value: 'one' },
-			{ value: 'two' },
-			{ value: 'three' }
+			{ value: 'pax' },
+			{ value: 'romana' }
 		])
 	})
 
@@ -50,16 +49,30 @@ describe(`parser`, () => {
 
 	it(`handles fielded terms`, () => {
 		expect(
-			parseQuery(`breed:lab`)
+			parseQuery(`mission:impossible`)
 		).toEqual([
-			{ field: 'breed', value: 'lab' }
+			{ field: 'mission', value: 'impossible' }
 		])
 
 		expect(
-			parseQuery(`breed:black lab`)
+			parseQuery(`mission:impossible iii`)
 		).toEqual([
-			{ field: 'breed', value: 'black' },
-			{ value: 'lab' }
+			{ field: 'mission', value: 'impossible' },
+			{ value: 'iii' }
+		])
+
+		expect(
+			parseQuery(`cold case:closed`)
+		).toEqual([
+			{ value: 'cold' },
+			{ field: 'case', value: 'closed' },
+		])
+
+		expect(
+			parseQuery(`crouching:tiger hidden:dragon`)
+		).toEqual([
+			{ field: 'crouching', value: 'tiger' },
+			{ field: 'hidden', value: 'dragon' }
 		])
 	})
 
@@ -97,6 +110,12 @@ describe(`parser`, () => {
 			{ value: 'orange', negated: true },
 			{ value: 'green' }
 		])
+
+		expect(
+			parseQuery(`donut:-filled`)
+		).toEqual([
+			{ field: 'donut', value: '-filled' }
+		])
 	})
 
 
@@ -111,200 +130,38 @@ describe(`parser`, () => {
 	})
 
 
-	xit(`permits escaping of special query characters`, () => {})
-	xit(`always returns an array`, () => {})
+	it(`permits escaping of special query characters`, () => {
+		expect(
+			parseQuery(`one\\ two`)
+		).toEqual([
+			{ value: 'one two' }
+		])
+
+		expect(
+			parseQuery(`one\\:two`)
+		).toEqual([
+			{ value: 'one:two' }
+		])
+
+		expect(
+			parseQuery(`one \\"two\\"`)
+		).toEqual([
+			{ value: 'one' },
+			{ value: '"two"' }
+		])
+	})
+
+
+	it(`always returns an array`, () => {
+		expect(Array.isArray( parseQuery(`word`) )).toBe(true)
+		expect(Array.isArray( parseQuery(``) )).toBe(true)
+	})
+
+
+	it(`throws if argument is not a string`, () => {
+		expect( ()=> parseQuery() ).toThrow()
+		expect( ()=> parseQuery(null) ).toThrow()
+	})
 
 
 })
-
-/*
-test('bare terms', (t) => {
-
-	t.deepEqual(
-		Parser(`ulysses`),
-		[
-			{ value: 'ulysses' }
-		]
-	)
-
-	t.deepEqual(
-		Parser(`pax romana`),
-		[
-			{ value: 'pax' },
-			{ value: 'romana' }
-		]
-	)
-
-})
-
-
-test('fielded terms', (t) => {
-
-	t.deepEqual(
-		Parser(`mission:impossible`),
-		[
-			{ field: 'mission', value: 'impossible' }
-		]
-	)
-
-	t.deepEqual(
-		Parser(`crouching:tiger hidden:dragon`),
-		[
-			{ field: 'crouching', value: 'tiger' },
-			{ field: 'hidden', value: 'dragon' }
-		]
-	)
-
-	t.deepEqual(
-		Parser(`cold case:closed`),
-		[
-			{ value: 'cold' },
-			{ field: 'case', value: 'closed' }
-		]
-	)
-
-})
-
-
-test('quoted terms', (t) => {
-
-	t.deepEqual(
-		Parser(`"new york"`),
-		[
-			{ value: 'new york' }
-		]
-	)
-
-	t.deepEqual(
-		Parser(`aka "chocolate jenga"`),
-		[
-			{ value: 'aka' },
-			{ value: 'chocolate jenga' }
-		]
-	)
-
-	t.deepEqual(
-		Parser(`author:"stephen king"`),
-		[
-			{ field: 'author', value: 'stephen king' }
-		]
-	)
-
-	t.deepEqual(
-		Parser(`"jesus christ":superstar`),
-		[
-			{ field: 'jesus christ', value: 'superstar' }
-		]
-	)
-
-	t.deepEqual(
-		Parser(`"once bitten":"twice shy"`),
-		[
-			{ field: 'once bitten', value: 'twice shy' }
-		]
-	)
-
-	t.deepEqual(
-		Parser(`trump "known as":drumpf`),
-		[
-			{ value: 'trump' },
-			{ field: 'known as', value: 'drumpf' }
-		]
-	)
-
-	t.deepEqual(
-		Parser(`"tara reid" scientist:"with glasses"`),
-		[
-			{ value: 'tara reid' },
-			{ field: 'scientist', value: 'with glasses' }
-		]
-	)
-
-})
-
-
-test('negative terms', (t) => {
-
-	t.deepEqual(
-		Parser(`-temperature`),
-		[
-			{ value: 'temperature', negated: true }
-		]
-	)
-
-	t.deepEqual(
-		Parser(`-"blood suckers"`),
-		[
-			{ value: 'blood suckers', negated: true }
-		]
-	)
-
-	t.deepEqual(
-		Parser(`reality -bites`),
-		[
-			{ value: 'reality' },
-			{ value: 'bites', negated: true }
-		]
-	)
-
-	t.deepEqual(
-		Parser(`-"closed session" interview`),
-		[
-			{ value: 'closed session', negated: true },
-			{ value: 'interview' }
-		]
-	)
-
-	t.deepEqual(
-		Parser(`-lang:en_UK`),
-		[
-			{ field: 'lang', value: 'en_UK', negated: true }
-		]
-	)
-
-	t.deepEqual(
-		Parser(`-band:"pink floyd"`),
-		[
-			{ field: 'band', value: 'pink floyd', negated: true }
-		]
-	)
-
-	t.deepEqual(
-		Parser(`-"party affiliation":republican`),
-		[
-			{ field: 'party affiliation', value: 'republican', negated: true }
-		]
-	)
-
-	t.deepEqual(
-		Parser(`-"date modified":"last week"`),
-		[
-			{ field: 'date modified', value: 'last week', negated: true }
-		]
-	)
-
-	t.deepEqual(
-		Parser(`spiderman -"played by":maguire`),
-		[
-			{ value: 'spiderman' },
-			{ field: 'played by', value: 'maguire', negated: true }
-		]
-	)
-
-	t.deepEqual(
-		Parser(`"the joker" -actor:"heath ledger"`),
-		[
-			{ value: 'the joker' },
-			{ field: 'actor', value: 'heath ledger', negated: true }
-		]
-	)
-
-	t.deepEqual(
-		Parser(`donut:-filled`),
-		[
-			{ field: 'donut', value: '-filled' }
-		]
-	)
-
-})
-*/
